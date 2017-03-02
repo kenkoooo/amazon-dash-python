9c72e12a3e7f5e75a36be7b09273bf605e9647b0
kenkoooo <kenkou.n@gmail.com>
2017-03-03 03:37:48 +0900

    mac address validator

--- dash_monitor.py
+++ dash_monitor.py
@@ -1,6 +1,8 @@
 import argparse
 from logging import getLogger, StreamHandler, DEBUG
 
+from scapy.all import sniff, ARP
+
 logger = getLogger(__name__)
 handler = StreamHandler()
 handler.setLevel(DEBUG)
@@ -8,8 +10,38 @@ logger.setLevel(DEBUG)
 logger.addHandler(handler)
 
 
+class DashMonitor:
+    def __init__(self, mac_address):
+        self.mac_address = mac_address
+
+    def start(self):
+        logger.info("Start monitoring: %s", self.mac_address)
+        sniff(prn=self.arp_monitor_callback, filter="arp", store=0)
+
+    def arp_monitor_callback(self, pkt):
+        """
+        show MAC address and IP address from sniffed packet
+        :param pkt: sniffed packet
+        :return: None
+        """
+        if ARP in pkt and pkt[ARP].op in (1, 2):
+            """
+            op==1: who has
+            誰がこの ip やねんみたいな感じで聞いてる
+            op==2: is at
+            ワイのこの mac address がその ip やみたいな感じで教えてる
+            """
+
+            # ARPSourceMACField: mac address
+            print("MAC Address: {}".format(pkt[ARP].hwsrc))
+
+            # SourceIPField: source ip address
+            print("IP Address: {}".format(pkt[ARP].psrc))
+
+
 def running_monitor(target):
-    logger.info("Start monitoring: %s", target)
+    monitor = DashMonitor(target)
+    monitor.start()
 
 
 def is_valid_mac_address(mac_address):
