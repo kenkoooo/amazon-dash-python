7522c77b41b24349565f305d39a3fba3a467f47d
kenkoooo <kenkou.n@gmail.com>
2017-03-03 14:28:14 +0900

    remove unused function

--- dash_monitor.py
+++ dash_monitor.py
@@ -10,25 +10,6 @@ handler.setFormatter(Formatter(fmt='%(asctime)-15s %(message)s'))
 logger.setLevel(DEBUG)
 logger.addHandler(handler)
 
-
-def is_valid_mac_address(mac_address):
-    """
-    validate mac address
-    :param mac_address:
-    :return:
-    """
-    nums = mac_address.split(":")
-    if len(nums) != 6:
-        return False
-    for num in nums:
-        try:
-            int(num, 16)
-        except ValueError as e:
-            logger.error(e)
-            return False
-    return True
-
-
 if __name__ == '__main__':
     buttons = load_config("config/config.json", logger=logger)
     monitor = DashMonitor(buttons=buttons, logger=logger)
